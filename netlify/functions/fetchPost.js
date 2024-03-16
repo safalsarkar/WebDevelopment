2b67cc32e55e904c8bd779b99bdf28d9e04c709d
safalsarkar <157328412+safalsarkar@users.noreply.github.com>
2024-03-16 13:51:48 -0700

    second commit

---
+++ netlify/functions/fetchPost.js
@@ -0,0 +1,24 @@
+exports.handler = async function(event, context) {
+    try {
+      const response = await fetch('https://dummyjson.com/posts/');
+      const data = await response.json();
+  
+      // Filter posts with more than 5 reactions
+      const filteredPosts = data.posts.filter(post => post.reactions > 5);
+  
+      return {
+        statusCode: 200,
+        body: JSON.stringify(filteredPosts)
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Failed to fetch posts' })
+      };
+    }
+  };
+
+
+
+
+   
